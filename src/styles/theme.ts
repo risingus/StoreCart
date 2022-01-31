54bc4215920076bbf05e48e100b42561b1bd083f
Gustavo <gustavo.it@outlook.com>
2022-01-31 08:52:49 -0300

    Adjusts
    1. Removed dark theme.
    2. Removed change theme button.
    3. Removed function to change theme.
    
    Obs: imgs from api have a white background, and its kind weird with the aplication in black mode.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -20,25 +20,3 @@ export const light = {
   }
 }
 
-export const dark = {
-  theme: 'dark',
-  colors: {
-    header: '#1b1b1f',
-    background_primary: '#f4f5f6',
-    background_secondary: '#ffffff',
-    text: '#7a7a80',
-    text_detail: '#aeaeb3',
-    title: '#47474d',
-    main: '#dc1637',
-    main_light: '#ffffff',
-    success: '#03b252',
-  },
-  fonts: {
-    regular: 'Roboto',
-    mid: 'RobotoMid',
-    bold: 'RobotoBold',
-    brand: 'Brand',
-    error: 'Error'
-  }
-}
-
